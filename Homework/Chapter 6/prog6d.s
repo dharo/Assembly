48787a9cde9f31a77f68a9bc3b85808c80839a91
David Haro <david.haro@me.com>
2015-09-17 14:53:27 -0700

    Update to notes

--- Homework/Chapter 6/prog6d.s
+++ Homework/Chapter 6/prog6d.s
@@ -1,13 +1,12 @@
-/* multiply two numbers R0=R1*R2	*/
+/*Multiply two numnbers R0=R1*R2*/
 
      .global _start
 _start:
 
-	MOV R1,#20
-	MOV R2,#5
-	MOV R3,#10
-	MLA R0,R1,R2,R3		@ R0 = (R1*R2)+R3
+     MOV R1,#20
+     MOV R2,#5
+     MOV R3,#10
+     MLA R0,R1, R2, R3
 
-	MOV R7,#1		@ EXIT THROUGH SYSCALL
-	SWI 0
-
+     MOV R7,#1
+     SWI 0
