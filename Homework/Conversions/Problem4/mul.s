24e12a2b20576d1643fc5421c524911d57afcbc6
dharo <dharo@student.rcc.edu>
2015-09-23 14:39:06 -0700

    Pi in Hex is aprox.0x324275 @ BP -20 WD 24 (meaning in binary, it is scaled up 20 places and its 24 bits long)

--- Homework/Conversions/Problem4/mul.s
+++ Homework/Conversions/Problem4/mul.s
@@ -5,8 +5,9 @@
 .global _start
 _start:
 	LDR R1, =0x6		@ BP 0  WD  8    radius = 6
-	LDR R2, =0x624275	@ BP-20 WD 24	 this is pi
-	MUL R0, R2, R1		@ BP-24 WD 32 this is the result
-	LSR R0, #24		@ BP 0 WD 8 <--result should be saved to 1 byte
+	LDR R2, =0x324275	@ BP-20 WD 24	 this is pi
+	MUL R3, R1,R1		@ R3=R1*R1	this is  radius^2
+	MUL R0, R2, R3		@ BP-20 WD 32 this is the result
+	LSR R0, #20		@ BP 0 WD 8 <--result should be saved to 1 byte
 	MOV R7, #1
 	SWI 0
