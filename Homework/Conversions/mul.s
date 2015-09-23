f1b90869f3644c6819320f1503f044b4cc7c1074
dharo <dharo@student.rcc.edu>
2015-09-23 11:35:58 -0700

    Update to mul.s

--- Homework/Conversions/mul.s
+++ Homework/Conversions/mul.s
@@ -1,9 +1,8 @@
 .global _start
 _start:
-	MOV R1, #48		@ BP0 WD6
-	MOV R2, #0x00000009	@ BP-4 WD4
-	MUL R0, R2, R1		@ BP-4 WD10 this is the result
-	LSR R0, #4		@ because the BP has been -4
-				@ it needs to be 0
+	MOV R1, #110		@ BP0 WD8
+	LDR R2, =0x517CC1	@ BP-24 WD24
+	MUL R0, R2, R1		@ BP-24 WD32 this is the result
+	LSR R0, #24		@ BP0 WD8
 	MOV R7, #1
 	SWI 0
