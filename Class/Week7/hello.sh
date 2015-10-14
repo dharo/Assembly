8f90085b9a5c999745c94c5bf40e94a090c66b76
dharo <dharo@student.rcc.edu>
2015-10-14 12:34:54 -0700

    In-Class Work

---
+++ Class/Week7/hello.sh
@@ -0,0 +1,4 @@
+echo "This is executing The Lab for October 14"
+./hello01
+./printf01
+./printf02
---
+++ Class/Week7/printf02_f.s
@@ -0,0 +1,11 @@
+/*
+mult_by_5 function
+*/
+mult_by_5:
+ ldr r1, address_of_return2 /* r1 ← &address_of_return */
+ str lr, [r1] /* *r1 ← lr */
+ add r0, r0, r0, LSL #2 /* r0 ← r0 + 4*r0 */
+ ldr lr, address_of_return2 /* lr ← &address_of_return */
+ ldr lr, [lr] /* lr ← *lr */
+ bx lr /* return from main using lr */
+address_of_return2 : .word return2
