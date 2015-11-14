05b42a4751a2b2be8f933b61ac280361d513fdc4
dharo <dharo@student.rcc.edu>
2015-11-14 01:14:32 -0800

    shell script for compiling

--- Assembly Midterm/Main/CSC11Midterm.sh
+++ Assembly Midterm/Main/CSC11Midterm.sh
@@ -1,2 +1,4 @@
-echo "Launching CSC 11 Midterm: ./midterm"
+echo "Compiling Midterm...\n"
+gcc -g -o midterm midterm.s prob1.s isp.s fibo.s
+echo "Midterm Compiled, executing midterm via command: ./midterm\n\n"
 ./midterm
