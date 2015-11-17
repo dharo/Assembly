22a92868d0cd39d44c6b6c50b20b44ded1bf4cb8
David Haro <david.haro@me.com>
2015-11-17 01:07:06 -0800

    zipped fib

--- Homework/Fibonacci/recursive/fib_rec.s
+++ Homework/Fibonacci/recursive/fib_rec.s
@@ -9,12 +9,10 @@ fib:
 	beq end		//if eq, mov #1 to R0 and branch to end
 
 	sub r0, r0, #1	//else r0 = r0 - #1
-	mov r4,r0 	//save r0 in R3
+	mov r4,r0 	//save r0 in R4 on the stack
 	bl fib		//recursive call  fibo(n-1)
-	mov r5, r0	//mov r0 to r5
+	mov r5, r0	//mov r0 to r5 on the stack
 	sub r0, r4, #1	// r0 = r4--
-	//mov r0,r2 	//load previous r0
-	//sub r0,r0,#1	//recursive call fibo(n-2)
 	bl fib
 	add r0,r0,r5
 end:
